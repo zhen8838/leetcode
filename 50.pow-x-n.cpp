c0eb87ac8a019226468f92917d6a409ce1d3161a
zhengqihang <597323109@qq.com>
2020-12-29 00:55:10 +0800

    12/28

--- 50.pow-x-n.cpp
+++ 50.pow-x-n.cpp
@@ -8,7 +8,25 @@
 class Solution {
  public:
   double myPow(double x, int n) {
-    
+    bool flag = true;
+    long long N = n;
+    if (N < 0) {
+      return 1 / pow(x, -N);
+    } else {
+      return pow(x, N);
+    }
+  }
+
+  double pow(double x, long long n) {
+    if (n == 0) {
+      return 1.;
+    }
+    double y = pow(x, n / 2);
+    if (n % 2 == 0) {
+      return y * y;
+    } else {
+      return y * y * x;
+    }
   }
 };
 // @lc code=end
