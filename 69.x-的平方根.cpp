325ba79c2b1959fe4c4ef8c7df2d54adceee5c03
zhengqihang <597323109@qq.com>
2020-12-30 01:51:34 +0800

    12/29

--- 69.x-的平方根.cpp
+++ 69.x-的平方根.cpp
@@ -3,13 +3,39 @@
  *
  * [69] x 的平方根
  */
-
+#include "commom.hpp"
 // @lc code=start
 class Solution {
-public:
-    int mySqrt(int x) {
-      
+ public:
+  int mySqrt(int x) {
+    if (x == 1) {
+      return 1;
     }
+
+    int left = 0, right = x / 2 + 1;
+    long t, mid;
+    while (left < right) {
+      mid = (left + right) / 2;
+      // ic(mid, left, right);
+      t = mid * mid;
+      if (t == x) {
+        // right = mid;
+        return mid;
+      } else if (t < x) {
+        left = mid + 1;
+      } else if (t > x) {
+        right = mid;
+      }
+    }
+    if (t > x) {
+      return mid - 1;
+    }
+    return mid;
+  }
 };
 // @lc code=end
-
+int main(int argc, char const* argv[]) {
+  Solution s;
+  ic(s.mySqrt(4));
+  return 0;
+}
