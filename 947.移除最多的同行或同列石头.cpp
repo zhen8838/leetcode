b6f411040176648719d89f6d4f41c178f31ec317
zhengqihang <597323109@qq.com>
2021-01-16 00:38:34 +0800

    1/15

--- 947.移除最多的同行或同列石头.cpp
+++ 947.移除最多的同行或同列石头.cpp
@@ -11,59 +11,75 @@
 
 class Solution {
  private:
-  unordered_map<int, pair<unordered_set<int>, unordered_set<int>>> > parent;
+  vector<int> xparent, yparent, parent;
 
  public:
   int removeStones(vector<vector<int>>& stones) {
-    auto x = unordered_set<int>(stones[0][0]);
-    auto y = unordered_set<int>(stones[0][1]);
-    parent[0] = make_pair(x, y);
-
-    for (size_t i = 1; i < stones.size(); i++) {
-      for (size_t j = 0; j < parent; j++) {
-        /* code */
-      }
-    }
+    unordered_map<int, int> xmap, ymap;
+    int xmax = 0, ymax = 0;
     for (size_t i = 0; i < stones.size(); i++) {
-      // int xroot = dofind();
-      // int yroot = dofind(yparent[i]);
-      // dounion()
-      parent[i] = min(xparent[i], yparent[i]);
+      if (xmap.find(stones[i][0]) == xmap.end()) {
+        xmap[stones[i][0]] = xmax++;
+        // xmax = i + 1;
+      }
+      if (ymap.find(stones[i][1]) == ymap.end()) {
+        ymap[stones[i][1]] = ymax++;
+        // ymax = i + 1;
+      }
+      xparent.push_back(xmap[stones[i][0]]);
+      yparent.push_back(ymap[stones[i][1]]);
     }
-
-    // ic(xmap, ymap);
-    ic(xparent, yparent, parent);
-    // unordered_set<int> uset;
-    // for (size_t i = 0; i < stones.size(); i++) {
-    //   if (xparent[i] == yparent[i]) {
-    //     uset.emplace(xparent[i]);
-    //     // ic(xparent[i], stones[xparent[i]]);
-    //   }
-    // }
-
-    // return stones.size() - uset.size();
-    // for (size_t i = 0; i < stones; i++) {
-    //   dounion(stones, stones[i][0], stones[i][1]);
-    // }
-    return 1;
+    for (size_t i = 0; i < xmax + ymax; i++) {
+      parent.push_back(i);
+    }
+    // ic(xparent, yparent);
+    // ic(parent);
+    for (size_t i = 0; i < stones.size(); i++) {
+      dounion(xparent[i], yparent[i] + xmax);
+    }
+    unordered_set<int> uset;
+    // uset.merge();
+    for (auto&& x : xparent) {
+      uset.emplace(dofind(x));
+    }
+    return stones.size() - uset.size();
   }
-  int dofind(int x) {
-    if (x != parent[x]) {
-      parent[x] = dofind(parent[x]);
+  //  private:
+  //   vector<int> parent;
+
+  //  public:
+  //   int removeStones(vector<vector<int>>& stones) {
+  //     constexpr int offset = 10000 + 1;
+  //     for (size_t i = 0; i < offset * 2; i++) {
+  //       parent.push_back(i);
+  //     }
+  //     for (auto&& stone : stones) {
+  //       dounion(stone[0], offset + stone[1]);
+  //     }
+  //     unordered_set<int> uset;
+  //     for (auto&& stone : stones) {
+  //       uset.insert(dofind(stone[0]));
+  //     }
+  //     return stones.size() - uset.size();
+  //   }
+  int dofind(int num) {
+    if (num != parent[num]) {
+      parent[num] = dofind(parent[num]);
     }
-    return parent[x];
+    return parent[num];
   }
   void dounion(int x, int y) {
-    int rootx = dofind(xparent[x]);
-    int rooty = dofind(yparent[y]);
+    // ic(x, y);
+    int rootx = dofind(x);
+    int rooty = dofind(y);
     parent[rooty] = rootx;
   }
 };
 // @lc code=end
 
 int main(int argc, char const* argv[]) {
-  // Mat<int> stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
-  Mat<int> stones = {{1, 0}, {0, 1}, {1, 1}};
+  Mat<int> stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+  // Mat<int> stones = {{1, 0}, {0, 1}, {1, 1}};
   Solution s;
   ic(s.removeStones(stones));
   return 0;
