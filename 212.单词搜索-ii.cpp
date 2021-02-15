aa822ae120afe6f35c98d747670c18bc6629da19
郑启航 <597323109@qq.com>
2021-02-16 01:29:54 +0800

    2/16

--- 212.单词搜索-ii.cpp
+++ 212.单词搜索-ii.cpp
@@ -3,13 +3,76 @@
  *
  * [212] 单词搜索 II
  */
+#include <unordered_map>
 
+#include "commom.hpp"
 // @lc code=start
+class TrieTree {
+ public:
+  unordered_map<char, TrieTree*> child;
+  bool isend;
+  TrieTree() : child(), isend(false) {}
+};
 class Solution {
-public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+ private:
+  int w, h;
+  vector<string> ans;
 
+ public:
+  vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    h = board.size(), w = board[0].size();
+    TrieTree *root = new TrieTree(), *node = nullptr;
+    for (auto&& word : words) {
+      node = root;
+      for (auto&& c : word) {
+        if (not node->child.count(c)) { node->child[c] = new TrieTree(); }
+        node = node->child[c];
+      }
+      node->isend = true;
     }
+    string s;
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) { dfs(board, root, s, x, y); }
+    }
+    delete root;
+    return ans;
+  }
+  bool dfs(vector<vector<char>>& board, TrieTree* root, string& s, int x,
+           int y) {
+    if (root->isend) {
+      if (not s.empty()) { ans.push_back(s); }
+      root->isend = false;
+      if (root->child.empty()) { return true; }
+    }
+    if (not isvalid(x, y)) { return false; }
+    if (not root->child.count(board[y][x])) { return false; }
+    s.push_back(board[y][x]);
+    board[y][x] = '*';
+    bool ret = dfs(board, root->child[s.back()], s, x + 1, y);
+    ret |= dfs(board, root->child[s.back()], s, x - 1, y);
+    ret |= dfs(board, root->child[s.back()], s, x, y + 1);
+    ret |= dfs(board, root->child[s.back()], s, x, y - 1);
+    if (ret and root->child.size() == 1) {
+      //加了一个裁剪多调试了一个小时..
+      delete root->child[s.back()];
+      root->child.erase(s.back());
+    }
+    board[y][x] = s.back();
+    s.pop_back();
+    return ret;
+  }
+
+  bool isvalid(int x, int y) { return 0 <= x and x < w and 0 <= y and y < h; }
 };
 // @lc code=end
 
+int main(int argc, char const* argv[]) {
+  Mat<char> board{{'o', 'a', 'a', 'n'},
+                  {'e', 't', 'a', 'e'},
+                  {'i', 'h', 'k', 'r'},
+                  {'i', 'f', 'l', 'v'}};
+  Vec<string> words{"oath", "pea", "eat", "rain", "hklf", "hf"};
+  Solution s;
+  ic(s.findWords(board, words));
+  return 0;
+}
---
+++ 561.数组拆分-i.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=561 lang=cpp
+ *
+ * [561] 数组拆分 I
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int arrayPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int res = 0;
+    for (int i = 0; i < (nums.size() / 2); i++) { res += nums[i * 2]; }
+    return res;
+  }
+};
+// @lc code=end
