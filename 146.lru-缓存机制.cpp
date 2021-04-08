60d7a3dbaaeef5c9d2099a1c5f3b70099f1f1ec2
郑启航 <597323109@qq.com>
2021-04-08 13:36:31 +0800

    4/8

--- 146.lru-缓存机制.cpp
+++ 146.lru-缓存机制.cpp
@@ -3,68 +3,83 @@
  *
  * [146] LRU 缓存机制
  */
-
+#include <unordered_map>
+using namespace std;
 // @lc code=start
-class LRUCache {
- private:
-  struct ListNode {
-    int k;
-    int v;
-    ListNode *next, *last;
-    ListNode(int _k, int _v) : k(_k), v(_v){};
-    ListNode() : v(0){};
-  };
-  ListNode *head, *tail;
-  int size;
-  unordered_map<int, ListNode*> umap;
+struct Node {
+  int key, val;
+  Node *last, *next;
+  Node(int _k, int _v) : key(_k), val(_v), last(nullptr), next(nullptr){};
+  Node() : key(-1), val(-1), last(nullptr), next(nullptr){};
+};
 
- public:
-  LRUCache(int capacity)
-      : size(capacity), head(new ListNode()), tail(new ListNode()) {
+struct List {
+  Node *head, *tail;
+  int size;
+  List() : head(new Node()), tail(new Node()), size(0) {
     head->next = tail;
-    head->last = tail;
     tail->last = head;
-    tail->next = head;
   }
 
+  void push_front(Node* node) {
+    node->next = head->next;
+    node->last = head;
+    head->next->last = node;
+    head->next = node;
+    size++;
+  }
+  Node* unlink(Node* node) {
+    node->next->last = node->last;
+    node->last->next = node->next;
+    node->last = nullptr;
+    node->next = nullptr;
+    size--;
+    return node;
+  }
+  Node* pop_back() { return unlink(tail->last); }
+};
+
+class LRUCache {
+ private:
+  int size, capacity;
+  unordered_map<int, Node*> keymap;
+  List list;
+
+ public:
+  LRUCache(int capacity) : size(0), capacity(capacity) {}
+
   int get(int key) {
-    if (umap.count(key)) {
-      movetohead(umap[key]);
-      return umap[key]->v;
-    }
-    return -1;
+    if (not keymap.count(key)) { return -1; }
+    Node* node = unlink(keymap[key]);
+    add(node);
+    return node->val;
   }
 
   void put(int key, int value) {
-    if (umap.count(key)) {
-      movetohead(umap[key]);
-      umap[key]->v = value;
+    Node* node = nullptr;
+    if (keymap.count(key)) {
+      node = unlink(keymap[key]);
+      node->val = value;
+      add(node);
     } else {
-      if (umap.size() == size) {
-        umap.erase(tail->last->k);
-        removenode(tail->last);
+      if (size == capacity) {
+        node = unlink(list.tail->last);
+        delete node;
       }
-      ListNode* node = new ListNode(key, value);
-      umap[key] = node;
-      inserttohead(node);
+      add(new Node(key, value));
     }
   }
-  void removenode(ListNode* node) {
-    node->last->next = node->next;
-    node->next->last = node->last;
-    node->next = nullptr;
-    node->last = nullptr;
-  }
 
-  void inserttohead(ListNode* node) {
-    node->next = head->next;
-    node->last = head;
-    node->next->last = node;
-    head->next = node;
+  Node* unlink(Node* node) {
+    keymap.erase(node->key);
+    list.unlink(node);
+    size--;
+    return node;
   }
-  void movetohead(ListNode* node) {
-    removenode(node);
-    inserttohead(node);
+  void add(Node* node) {
+    list.push_front(node);
+    keymap[node->key] = node;
+    size++;
   }
 };
 
