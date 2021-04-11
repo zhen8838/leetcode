00feb2b5db0e0821c9a535f43a8faf52aaff0531
郑启航 <597323109@qq.com>
2021-04-11 10:57:30 +0800

    4/11

---
+++ 263.丑数.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=cpp
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool isUgly(int n) {
+    if (n == 0) { return false; }
+    if (n == 1) { return true; }
+    if (n % 2 == 0) { return isUgly(n / 2); }
+    if (n % 3 == 0) { return isUgly(n / 3); }
+    if (n % 5 == 0) { return isUgly(n / 5); }
+    return false;
+  }
+};
+// @lc code=end
--- 智能指针.cc
+++ 智能指针.cc
@@ -1,5 +1,5 @@
 #include <memory>
-
+#include <typeinfo>
 #include "commom.hpp"
 
 class Report {
@@ -16,6 +16,13 @@ int main(int argc, char const* argv[]) {
   {
     auto_ptr<Report> ps(new Report("auto_ptr"));
     ps->comment();
+    int myint = 50;
+    std::string mystr = "string";
+    double* mydoubleptr = nullptr;
+
+    std::cout << "myint has type: " << typeid(myint).name() << '\n'
+              << "mystr has type: " << typeid(mystr).name() << '\n'
+              << "mydoubleptr has type: " << typeid(mydoubleptr).name() << '\n';
   }
   {
     shared_ptr<Report> ps(new Report("shared_ptr"));
